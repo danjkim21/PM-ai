63185dc883c257100ab8feb7e0bdf0909a0cb40d
danjkim21 <dan.jkim21@gmail.com>
2024-05-07 13:35:44 -0400

    created next auth provider context

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,10 +1,16 @@
-export default async function AuthLayout({
+"use client";
+
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+
+export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  //TODO: implement redirect if user has active session
-  // eg. if (session) redirect("/dashboard");
+  const { data: session, status } = useSession();
+
+  if (status === "authenticated") redirect("/dashboard");
 
   return <div className="bg-muted h-screen pt-8">{children}</div>;
 }
