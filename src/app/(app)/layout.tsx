5d84463fdd91266b57f38494ed99c8b9f898a3fb
danjkim21 <dan.jkim21@gmail.com>
2024-05-19 13:10:39 -0400

    hotfix- add app route protection if unauth

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,14 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 import Sidebar from "~/components/sidebar/Sidebar";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
+  const { status } = useSession();
+
+  if (status === "unauthenticated") redirect("/");
+
   return (
     <div className="grid h-screen w-full pl-[56px]">
       <Sidebar />
