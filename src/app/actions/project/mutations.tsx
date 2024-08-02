80ea8910fb36a3576e66f15a39a4bd11e3b93238
danjkim21 <dan.jkim21@gmail.com>
2024-08-02 09:47:28 -0400

    created updateProject mutation action

--- src/app/actions/project/mutations.tsx
+++ src/app/actions/project/mutations.tsx
@@ -7,9 +7,11 @@ import { db } from "~/server/db";
 import {
   type NewProjectParams,
   type ProjectId,
+  UpdateProjectParams,
   insertProjectSchema,
   projectIdSchema,
   projects,
+  updateProjectSchema,
 } from "~/server/db/schema";
 
 export const createProject = async (project: NewProjectParams) => {
@@ -22,6 +24,42 @@ export const createProject = async (project: NewProjectParams) => {
 
   try {
     const [p] = await db.insert(projects).values(newProject).returning();
+
+    return { project: p };
+  } catch (err) {
+    const message = (err as Error).message ?? "Error, please try again";
+    console.error(message);
+    throw { error: message };
+  }
+};
+
+export const updateProject = async (
+  id: ProjectId,
+  project: UpdateProjectParams,
+) => {
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user || !session.user.id) {
+    throw new Error("User not authenticated");
+  }
+
+  const { id: projectId } = projectIdSchema.parse({ id });
+  const newProject = updateProjectSchema.parse({
+    ...project,
+    createdById: session?.user?.id,
+  });
+
+  try {
+    const [p] = await db
+      .update(projects)
+      .set({ ...newProject, updatedAt: new Date() })
+      .where(
+        and(
+          eq(projects.id, projectId),
+          eq(projects.createdById, session.user.id),
+        ),
+      )
+      .returning();
 
     return { project: p };
   } catch (err) {
