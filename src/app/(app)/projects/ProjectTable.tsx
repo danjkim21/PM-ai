22f3073a8d09e8863d81da4c4bca76245391d0b2
danjkim21 <dan.jkim21@gmail.com>
2024-08-02 08:56:26 -0400

    created delete mutation action and moved project table to separate component

---
+++ src/app/(app)/projects/ProjectTable.tsx
@@ -0,0 +1,63 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "~/components/ui/table";
+import ProjectTableRow from "./ProjectTableRow";
+import { getProjects } from "~/app/actions/project/queries";
+
+export default async function ProjectTable() {
+  const { projects } = await getProjects();
+
+  return (
+    <Card x-chunk="dashboard-06-chunk-0">
+      <CardHeader>
+        <CardTitle>Projects</CardTitle>
+        <CardDescription>
+          Manage your Projects and generate tickets.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="hidden w-[100px] sm:table-cell">
+                <span className="sr-only">Image</span>
+              </TableHead>
+              <TableHead>Name</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead className="hidden md:table-cell">Created at</TableHead>
+              <TableHead>
+                <span className="sr-only">Actions</span>
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {projects?.map((project) => (
+              <ProjectTableRow key={project.id} project={project} />
+            ))}
+          </TableBody>
+        </Table>
+      </CardContent>
+      <CardFooter>
+        <div className="text-muted-foreground text-xs">
+          Showing{" "}
+          <strong>
+            {projects?.length}-{projects?.length}
+          </strong>{" "}
+          of <strong>{projects?.length}</strong> Projects
+        </div>
+      </CardFooter>
+    </Card>
+  );
+}
--- src/app/actions/project/mutations.tsx
+++ src/app/actions/project/mutations.tsx
@@ -1,11 +1,14 @@
 "use server";
 
+import { and, eq } from "drizzle-orm";
 import { getServerSession } from "next-auth";
 import { authOptions } from "~/server/auth";
 import { db } from "~/server/db";
 import {
   type NewProjectParams,
+  type ProjectId,
   insertProjectSchema,
+  projectIdSchema,
   projects,
 } from "~/server/db/schema";
 
@@ -18,9 +21,37 @@ export const createProject = async (project: NewProjectParams) => {
   });
 
   try {
-    const [a] = await db.insert(projects).values(newProject).returning();
+    const [p] = await db.insert(projects).values(newProject).returning();
 
-    return { project: a };
+    return { project: p };
+  } catch (err) {
+    const message = (err as Error).message ?? "Error, please try again";
+    console.error(message);
+    throw { error: message };
+  }
+};
+
+export const deleteProject = async (id: ProjectId) => {
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user || !session.user.id) {
+    throw new Error("User not authenticated");
+  }
+
+  const { id: projectId } = projectIdSchema.parse({ id });
+
+  try {
+    const [p] = await db
+      .delete(projects)
+      .where(
+        and(
+          eq(projects.id, projectId),
+          eq(projects.createdById, session.user.id),
+        ),
+      )
+      .returning();
+
+    return { project: p };
   } catch (err) {
     const message = (err as Error).message ?? "Error, please try again";
     console.error(message);
